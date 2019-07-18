4c7259aa3206629698b5ed5d4f4a9a9cddb09a89
Jacob Henning <jacobshenning@gmail.com>
2019-07-18 13:36:11 -0700

    Updated structure page

--- resources/js/react/Resources/Public/Structure.js
+++ resources/js/react/Resources/Public/Structure.js
@@ -23,13 +23,13 @@ class Structure extends Component {
               <div className="alert alert-danger" role="alert">
                 <h4> App Component </h4>
                 <hr />
-                <p> The app component serves as the container for the react app. It imports the router, componets, and then compiles the main view. Since this app doesn't use redux, the app container is pretty simple </p>
+                <p> The app component serves as the container for the React App. It imports the Router, React Components, and compiles them into a view. The app container is simple without the use of a <mark>state</mark> library like Redux. The only complexity left in this app container is from grabbing an <mark>api_token cookie</mark> and using the React Router</p>
               </div>
               <div className="alert alert-primary" role="alert">
                 <h4> Resource Components </h4>
                 <hr />
-                <p> Resource components fall into a tree of directories inside the react app file structure. Each resource component is contained in a topical directory. Each topical directory is directly tied to a Controller in the Laravel API. An example structure is below</p>
-                <p><strong>ProjectController</strong></p>
+                <p> Resource Components are category-based components. And by category-based I mean both; Resource Components fall under a category and Resource Components sit in a category folder. Each category folder is based on a Laravel Controller, directly linking Resource Components to API Controllers, which helps keep consistent namespacing across the entire application. See example below.</p>
+                <p><strong>Laravel Project Controller</strong></p>
                 <ul>
                   <li>CreateProject()</li>
                   <li>EditProject()</li>
@@ -39,27 +39,27 @@ class Structure extends Component {
                 </ul>
                 <p><strong>React App Resource Directory</strong></p>
                 <ul>
-                  <li>Resource/Projects/CreateProject.js</li>
-                  <li>Resource/Projects/EditProject.js</li>
-                  <li>Resource/Projects/CreateProject.js</li>
-                  <li>Resource/Projects/ViewProject.js</li>
+                  <li>Resource/Project/CreateProject.js</li>
+                  <li>Resource/Project/EditProject.js</li>
+                  <li>Resource/Project/CreateProject.js</li>
+                  <li>Resource/Project/ViewProject.js</li>
                 </ul>
-                <p> Notice that there is no component for deleting a project. Sometimes a resource component can be responsible for multiple resource interactions. In this case, users may end up deleteing Projects from within the browse or view components.</p>
+                <p>Notice that the Project Controller has a function for deleting projects even though the Resource Component for deleting a project doesn't exist. Resource Components are allowed to handle multiple Controller <mark>endpoints</mark>. There are usually more Resource Components then functions in the corresponding Controller.</p>
               </div>
               <div className="alert alert-success" role="alert">
                 <h4> Request Components </h4>
                 <hr />
-                <p>Remember how I said Resource Components will sometimes handle multiple resource interactions? That's what makes request components different. Request components are designed to do one thing: Facilitate a single request. Request components should be used anytime you need to validate request data and might be sending information back and forth frequently.</p>
+                <p>Request Components are for facilitating a specific request. Unlike Resource Components, Request Components only handle one <mark>endpoint</mark>. Request Components are usually associated with a Laravel RequestForm object in the API. Having a single component setup for validation simplifies validation concerning unique data.</p>
               </div>
               <div className="alert alert-warning" role="alert">
                 <h4> Section Components </h4>
                 <hr />
-                <p>Section components are low functioning components designed to be used over and over again. Section components are going to be things like navbars and footers, and rarely carry any special functionality.</p>
+                <p>Section Components jobs are somewhat general. Ideally, these components will be kept simple and only used to prevent repeating code. However, in some circumstances, a Section Component can contain a Request Component or <mark>programmatic rendering</mark> based on <mark>state</mark>. Usually, it's for rendering navigation, footers, and so on. </p>
               </div>
               <div className="alert alert-info" role="alert">
                 <h4> Components </h4>
                 <hr />
-                <p>Components are the simplest form of component. Their only job is to display data passed into them. They don't bind to anything, they aren't updating anything, they're just displaying data on the user interface.</p>
+                <p>The base Component is designed to do nothing but render. No data <mark>binding</mark>. No cookie interaction. No API requests. The base component receives <mark>props</mark>, calculates its output, and then renders.</p>
               </div>
             </div>
           </div>
